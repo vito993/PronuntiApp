4b8f3c9e060ef1a23a290cf4388e94f5018feb70
RobertoGuastamacchia <roberto.guastamacchia@external.eni.com>
2023-12-30 11:22:42 +0100

    genitore home

---
+++ app/src/main/java/com/firefighters/pronuntiapp/HomeGenitori.kt
@@ -0,0 +1,11 @@
+package com.firefighters.pronuntiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeGenitori : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_genitori)
+    }
+}
